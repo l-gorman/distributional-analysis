975129955e986c38b8d8191f4900bac57155fc30
l-gorman <lgorman@turing.ac.uk>
2022-10-06 14:53:42 +0100

    removing test partition

--- r_submit_distributional_test.sh
+++ r_submit_distributional_test.sh
@@ -2,10 +2,7 @@
 
 #SBATCH --mail-user=[lg14410@bristol.ac.uk]
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_80
-
-
 #SBATCH --job-name=test_job
-#SBATCH --partition=test
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
@@ -15,16 +12,16 @@
 
 cd "${SLURM_SUBMIT_DIR}"
 
-echo Running on host "$(hostname)" \n
-echo Time is "$(date)" \n
-echo Directory is "$(pwd)" \n
-echo Slurm job ID is "${SLURM_JOBID}" \n
-echo This jobs runs on the following machines: \n
+echo "Running on host $(hostname) \n"
+echo "Time is $(date) \n"
+echo "Directory is $(pwd) \n"
+echo "Slurm job ID is ${SLURM_JOBID} \n"
+echo "This jobs runs on the following machines: \n"
 echo "${SLURM_JOB_NODELIST}"
 
-echo Keep track of job by entering "sacct -j ${SLURM_JOBID} " \n
-echo Cancel your job by entering "scancel ${SLURM_JOBID} " \n
-echo More details on submitting jobs here https://www.acrc.bris.ac.uk/protected/hpc-docs/job_types/ \n
+echo "Keep track of job by entering sacct -j ${SLURM_JOBID}  \n"
+echo "Cancel your job by entering scancel ${SLURM_JOBID}  \n"
+echo "More details on submitting jobs here https://www.acrc.bris.ac.uk/protected/hpc-docs/job_types/ \n"
 
 module add languages/r/4.1.0
 
@@ -35,9 +32,8 @@ export OMP_NUM_THREADS=4
 for SAMPLE in 100 500  
 do
   for ITER in 2000 5000  
-    Rscript skew-normal-fit.R -n $SAMPLE -i $ITER -d "$d"
+    Rscript skew-normal-fit.R -n $SAMPLE -i $ITER -d $d
   done
-    
 done
 
 
