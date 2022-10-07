0b898339d4748fdaf15c4341dcbd32aef3d2b6f4
l-gorman <lgorman@turing.ac.uk>
2022-10-07 09:32:56 +0100

    adding extra matrix values

--- r_submit_distributional_test.sh
+++ r_submit_distributional_test.sh
@@ -25,13 +25,13 @@ echo "More details on submitting jobs here https://www.acrc.bris.ac.uk/protected
 
 module add languages/r/4.1.0
 
-d=$(date +%Y-%m-%d)
+# d=$(date +%Y-%m-%d)
 
 export OMP_NUM_THREADS=4
 
-for SAMPLE in 100 500  
+for SAMPLE in 100 200 500 
 do
-  for ITER in 2000 5000 
+  for ITER in 2000 5000 10000
   do
     Rscript skew-normal-fit.R -n $SAMPLE -i $ITER -d $d
   done
